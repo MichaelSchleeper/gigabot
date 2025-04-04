178186ba27653c3c1e9850b8037cc41a0e1f822f
Tyler Lewis <tglewis247@gmail.com>
2025-04-04 15:40:42 -0500

    added correct closing

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const app = express();
+process.on('SIGINT', shutDownServer);
+process.on('SIGTERM', shutDownServer);
 dotenv.config();
+server = app.listen(process.env.PORT);
 app.get('/', (req, res)=>{
     res.send(`server up on port: ${process.env.PORT}`);
-})
-app.listen(process.env.PORT);
+});
+function shutDownServer(){
+    console.log("Shutting down server"); 
+    server.close()
+    console.log("Server shut down!\nGoodbye!"); 
+    process.exit(0);
+}
 module.exports = app;
